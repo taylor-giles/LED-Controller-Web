ae72ed0ae9b2230495a57855f41ea7ead54e5f9a
taylor-giles <taylor.g1823@gmail.com>
2022-11-01 16:06:51 -0400

    GradientSelector select behavior

--- led-controller/src/lib/GradientSelector.svelte
+++ led-controller/src/lib/GradientSelector.svelte
@@ -3,37 +3,72 @@
     import { createEventDispatcher } from "svelte";
     import IconButton, { Icon } from "@smui/icon-button";
     import DeleteIcon from "svelte-material-icons/Delete.svelte";
+    import CheckIcon from "svelte-material-icons/Check.svelte";
 
     const dispatch = createEventDispatcher();
 
     export let gradients = [];
+    export let chosenGradient = {};
 
     function deleteGradient(gradient) {
         dispatch("delete", gradient);
     }
 
-    function selectGradient(gradient){
-        dispatch("select", gradient)
+    function selectGradient(gradient) {
+        chosenGradient = gradient;
     }
 </script>
 
 <div id="main">
     {#each gradients as gradient}
-        <div class="element">
-            {gradient.name}
-            <div class="element-content">
-                <div class="gradient-view">
-                    <GradientView bind:colors={gradient.colors} height="40px"/>
+        {#if gradient !== chosenGradient}
+            <div class="element">
+                {gradient.name}
+                <div class="element-content">
+                    <div class="gradient-view">
+                        <GradientView
+                            bind:colors={gradient.colors}
+                            height="40px"
+                        />
+                    </div>
+
+                    <button
+                        class="button"
+                        on:click={() => selectGradient(gradient)}>Select</button
+                    >
+                    <IconButton
+                        on:click={() => deleteGradient(gradient)}
+                        class="icon-button"
+                    >
+                        <DeleteIcon
+                            width="30px"
+                            height="30px"
+                            color="#f0f0f0"
+                        />
+                    </IconButton>
+                </div>
+            </div>
+        {:else}
+            <div class="element selected">
+                <div class="selected-header">
+                    {gradient.name}
+                    <div class="selected-indicator">
+                        Selected
+                        <CheckIcon height="14px"/>
+                    </div>
                 </div>
                 
-                <button class="button" on:click={()=>selectGradient(gradient)}>Select</button>
-                <IconButton on:click={()=> deleteGradient(gradient)} class="icon-button">
-                    <DeleteIcon width="30px" height="30px"/>
-                </IconButton>
+
+                <div class="element-content">
+                    <div class="gradient-view">
+                        <GradientView
+                            bind:colors={gradient.colors}
+                            height="40px"
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
-        
-       
+        {/if}
     {/each}
 </div>
 
@@ -42,25 +77,36 @@
         width: 100%;
     }
 
-    .element{
+    .element {
         border-top: 1px solid #a0a0a0;
-        border-bottom: 1px solid #a0a0a0;
-        margin-bottom: -1px;
         padding-top: 12px;
         padding-bottom: 12px;
     }
+    .element.selected {
+        background-color: #f0f0f0;
+        color: #222;
+        padding-inline: 15px;
+        padding-bottom: 10px;
+        border-radius: 8px;
+    }
 
-    .element-content{
+    .element-content {
         display: flex;
         padding-bottom: 10px;
+        align-items: center;
     }
 
-    .gradient-view{
+    .selected-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .gradient-view {
         flex: 1;
-        margin-right: 10px;
     }
 
-    .button{
-        margin-right: 10px;
+    .button {
+        margin-inline: 10px;
     }
 </style>
