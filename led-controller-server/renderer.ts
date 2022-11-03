3ab8de9c6159964e3b4a8857e8170dd2c6a5a69a
taylor-giles <taylor.g1823@gmail.com>
2022-11-03 13:43:33 -0400

    Add types for WS server

--- led-controller-server/renderer.ts
+++ led-controller-server/renderer.ts
@@ -1,7 +1,9 @@
+import WebSocket from "ws";
+import { WebSocketServer } from 'ws';
 import { IDisplay } from "./schemas";
 
-export function handleWSConnection(ws){
-    ws.on('message', function message(data) {
+export function handleWSConnection(ws: WebSocket){
+    ws.on('message', function message(data: WebSocket.RawData) {
         ws.send("I received your message");
         console.log('received: %s', data);
     });
