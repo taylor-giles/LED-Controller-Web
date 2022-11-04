4ee0b0a97a6be9ca9b5cf3250fe5bfdb1e492ff2
taylor-giles <taylor.g1823@gmail.com>
2022-11-04 15:46:21 -0400

    Server fix (for real)

--- led-controller-server/server.ts
+++ led-controller-server/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 });
-app.use('/', router)
+app.use('/api', router)
 
 const DB_URI = process.env.DB_URI ?? "";
 mongoose.connect(DB_URI).catch(
